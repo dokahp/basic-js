ae8b97d91bc8d1c8aad0521c3930c18ce629bfee
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-03-25 01:08:49 +0300

    feat: add extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  if (!options.hasOwnProperty('separator')) {options.separator = '+'}
+  if (!options.hasOwnProperty('addition')) {options.addition = ''}
+  if (!options.hasOwnProperty('additionRepeatTimes')) {options.additionRepeatTimes = 1}
+  if (!options.hasOwnProperty('repeatTimes')) {options.repeatTimes = 1}    
+  if (!options.hasOwnProperty('additionSeparator')) {options.additionSeparator = '|'}    
+  let addition = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes)
+  if (options.additionSeparator.length) {
+      addition = addition.slice(0, -options.additionSeparator.length)
+  }
+  return ((str + addition) + options.separator).repeat(options.repeatTimes).slice(0, -options.separator.length)
 }
 
 module.exports = {
